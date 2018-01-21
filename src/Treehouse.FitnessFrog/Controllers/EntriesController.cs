9b56b3b945fc981d2191929e35c703edd548ccb9
Cory <Coryf65@gmail.com>
2018-01-21 16:01:13 -0600

    Added validation of summary section

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -58,6 +58,8 @@ namespace Treehouse.FitnessFrog.Controllers
         public ActionResult Add(Entry entry)
         {
 
+            //ModelState.AddModelError("", "This is a Global Message");
+
             //If there aren't any "Duration" field validation erros than make sure the duration is greater than "0".
             if (ModelState.IsValidField("Duration") && entry.Duration <= 0)
             {
